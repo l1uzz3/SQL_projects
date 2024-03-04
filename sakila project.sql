2f61bf5469670621d6d6be0f7ee65b34feaab87f
Stefan Liute <139389824+l1uzz3@users.noreply.github.com>
2024-03-04 22:46:37 +0200

    Update sakila project.sql

--- sakila project.sql
+++ sakila project.sql
@@ -78,12 +78,30 @@ GROUP BY YEAR(rental_date), MONTH(rental_date)
 ORDER BY YEAR(rental_date) ASC, MONTH(rental_date) ASC;
 
 	--  the most popular film categories among customers in Bucuresti and Botosani (the only cities from Romania)
-SELECT 
-
+SELECT cat.name AS category_name, COUNT(rental.rental_id) AS rental_count -- selecting cat.name and the number of rentals from rental table
+FROM category AS cat -- from category table 
+JOIN film_category AS fc ON cat.category_id = fc.category_id -- connect film category to category tables by category_id first
+JOIN film AS f ON fc.film_id = f.film_id -- connect film table on film_category table with film_id
+JOIN inventory AS inv ON f.film_id = inv.film_id -- connect inventory table to film table by film_id
+JOIN rental AS rental ON inv.inventory_id = rental.inventory_id -- connect rental table to inventory table by inventory_id
+JOIN customer AS cust ON rental.customer_id = cust.customer_id -- connect customer table to rental table by customer_id
+JOIN address AS addr ON cust.address_id = addr.address_id -- connect address table on customer table by address_id
+JOIN city AS city ON addr.city_id = city.city_id -- connect city table on address table by city_id
+WHERE city.city = 'Bucuresti' -- the where clause to set the specific city aka Bucuresti
+GROUP BY cat.name -- Using GROUP BY to GROUp them by the name of the category of the film
+ORDER BY rental_count DESC; -- ORDER BY to sort them descendingly
 
 -- 3rd part: Actor analysis
+	-- List all actors by the number of films they've appeared in
+SELECT a.actor_id, a.first_name, a.last_name, COUNT(fa.film_id) AS 'Number of films' FROM actor as a
+JOIN  film_actor as fa 
+ON a.actor_id = fa.actor_id
+GROUP BY a.actor_id
+ORDER BY COUNT(fa.film_id) DESC;
 
--- 4th part: Rental performance
-
--- 5th part: Store and Staff Analysis
-
+	-- all films where a particular actor has appeared
+SELECT a.first_name, a.last_name, f.title FROM actor as a
+INNER JOIN film_actor as fa ON a.actor_id = fa.actor_id -- connecting film_actor to actor table by actor_id
+INNER JOIN film as f ON fa.film_id = f.film_id -- connecting film table to film_actor table by film_id
+WHERE a.first_name = 'Warren' AND a.last_name = 'Jackman'; -- specific random actor
+	
